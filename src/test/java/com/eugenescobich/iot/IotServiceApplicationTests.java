edb7010ec0764c84c4998a4dec11d2a6e5d9b4d3
Evgheni Scobici <evghenii.scobici@endava.com>
2023-04-11 14:45:19 +0300

    First Commit

---
+++ src/test/java/com/eugenescobich/iot/IotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eugenescobich.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
